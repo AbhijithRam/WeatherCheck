b56e50b10a5b5ed9062be1133b1e5c77154fcb96
Abhijith Ram <abhijithudma34@gmail.com>
2025-10-28 23:51:19 +0530

    added Time paramter

--- DTOs/WeatherForecastDto.cs
+++ DTOs/WeatherForecastDto.cs
@@ -8,6 +8,7 @@ namespace MyDotnet8Api.DTOs
         public DateTime Date { get; set; }
         public int TemperatureC { get; set; }
         public string? Summary { get; set; }
+        public DateTime? Time { get; set; }
 
         // to be added time permitting
     }
