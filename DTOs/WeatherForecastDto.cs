40f7c5dbea1994b6891c6d6134695326522847a3
Abhijith Ram <abhijithudma34@gmail.com>
2025-10-28 23:35:23 +0530

    xml for time updation

--- DTOs/WeatherForecastDto.cs
+++ DTOs/WeatherForecastDto.cs
@@ -8,5 +8,7 @@ namespace MyDotnet8Api.DTOs
         public DateTime Date { get; set; }
         public int TemperatureC { get; set; }
         public string? Summary { get; set; }
+
+        // to be added time permitting
     }
 }
